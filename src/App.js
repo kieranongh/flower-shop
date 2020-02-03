c39dca072367d7b8559fb7c08b1a64c9de994599
Kieran L <none>
2020-02-03 20:21:27 +1100

    Clear unused initial files

--- src/App.js
+++ src/App.js
@@ -3,27 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 import Rose from './models/Rose'
 
-function App() {
+const App = () => {
   let rose1 = new Rose(25)
   console.log(`rose1 => `, rose1)
-  console.log(`rose1.quantity => `, rose1.quantity)
+  console.log(`rose => `, rose)
   console.log(`rose1.order => `, rose1.order)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
     </div>
   );
 }
