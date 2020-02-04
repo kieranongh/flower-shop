dd2c56638bb8d63295415a9857f5ab750eb7bf47
Kieran L <none>
2020-02-04 22:14:22 +1100

    Create tests for remaining Flower order types

--- src/__tests__/LilyOrder.test.js
+++ src/__tests__/LilyOrder.test.js
@@ -1,36 +1,41 @@
-import RoseOrder from '../models/RoseOrder'
+import LilyOrder from '../models/LilyOrder'
 
 test('Create an instance, check simple properties', () => {
-  const order = new RoseOrder(25)
+  const order = new LilyOrder(15)
   expect(order).toBeDefined()
   expect(order).toHaveProperty("uuid")
-  expect(order).toHaveProperty("code", "R12")
-  expect(order).toHaveProperty("quantity", 25)
+  expect(order).toHaveProperty("code", "L09")
+  expect(order).toHaveProperty("quantity", 15)
   expect(order).toHaveProperty("bundles", [
     {
-      quantity: 10,
-      price: 1299
+      quantity: 9,
+      price: 2495
     },
     {
-      quantity: 5,
-      price: 699
+      quantity: 6,
+      price: 1695
+    },
+    {
+      quantity: 3,
+      price: 995
     }
   ])
   expect(order.error).toBeNull()
 })
 
 test('Create an instance, check valid orders are correct', () => {
-  const order = new RoseOrder(25)
+  const order = new LilyOrder(15)
   expect(order).toBeDefined()
   // property that's used to calculate orders
   expect(order).not.toHaveProperty("remaining")
-  expect(order).toHaveProperty("order.10", { quantity: 2, subtotalPrice: 2598 })
-  expect(order).toHaveProperty("order.5", { quantity: 1, subtotalPrice: 699 })
-  expect(order).toHaveProperty("totalPrice", 3297)
+  expect(order).toHaveProperty("order.9", { quantity: 1, subtotalPrice: 2495 })
+  expect(order).toHaveProperty("order.6", { quantity: 1, subtotalPrice: 1695 })
+  expect(order).toHaveProperty("order.3", { quantity: 0, subtotalPrice: 0 })
+  expect(order).toHaveProperty("totalPrice", 4190)
 })
 
 test('Create an instance, check unsolveable orders are handled', () => {
-  const order = new RoseOrder(22)
+  const order = new LilyOrder(13)
   expect(order).toBeDefined()
   expect(order).toHaveProperty("order", {})
   expect(order).toHaveProperty("totalPrice", 0)
--- src/__tests__/TulipOrder.test.js
+++ src/__tests__/TulipOrder.test.js
@@ -1,36 +1,40 @@
-import RoseOrder from '../models/RoseOrder'
+import TulipOrder from '../models/TulipOrder'
 
 test('Create an instance, check simple properties', () => {
-  const order = new RoseOrder(25)
+  const order = new TulipOrder(13)
   expect(order).toBeDefined()
   expect(order).toHaveProperty("uuid")
-  expect(order).toHaveProperty("code", "R12")
-  expect(order).toHaveProperty("quantity", 25)
+  expect(order).toHaveProperty("code", "T58")
+  expect(order).toHaveProperty("quantity", 13)
   expect(order).toHaveProperty("bundles", [
     {
-      quantity: 10,
-      price: 1299
+      quantity: 9,
+      price: 1699
     },
     {
       quantity: 5,
-      price: 699
+      price: 995
+    },
+    {
+      quantity: 3,
+      price: 595
     }
   ])
   expect(order.error).toBeNull()
 })
 
 test('Create an instance, check valid orders are correct', () => {
-  const order = new RoseOrder(25)
+  const order = new TulipOrder(13)
   expect(order).toBeDefined()
   // property that's used to calculate orders
   expect(order).not.toHaveProperty("remaining")
-  expect(order).toHaveProperty("order.10", { quantity: 2, subtotalPrice: 2598 })
-  expect(order).toHaveProperty("order.5", { quantity: 1, subtotalPrice: 699 })
-  expect(order).toHaveProperty("totalPrice", 3297)
+  expect(order).toHaveProperty("order.5", { quantity: 2, subtotalPrice: 1990 })
+  expect(order).toHaveProperty("order.3", { quantity: 1, subtotalPrice: 595 })
+  expect(order).toHaveProperty("totalPrice", 2585)
 })
 
 test('Create an instance, check unsolveable orders are handled', () => {
-  const order = new RoseOrder(22)
+  const order = new TulipOrder(11)
   expect(order).toBeDefined()
   expect(order).toHaveProperty("order", {})
   expect(order).toHaveProperty("totalPrice", 0)
